591fe5438d61bc56caa1a7ae89f2cd4350b70156
jacobjolibois <jacobjolibois@hey.com>
2021-12-20 15:29:58 -0600

    fleshed out the props and functions for the Class Card component

--- src/components/class-card.js
+++ src/components/class-card.js
@@ -19,17 +19,53 @@ const StyledCardDetail = styled.div`
 
 // Build component
 function ClassCard(props) {
+	// Deconstruct props
+	const { delete, register, registered, type, start, duration, intensity, location, currentCt, maxCt } = props;
+
+	// Card functions
+	const onDelete = event => {
+		// Delete function
+		// Should trigger a DELETE axios request
+		delete();
+	}
+	const onRegister = event => {
+		// Register function
+		// Should toggle the 'registered' boolean
+		register();
+	}
+
 	// Return elements
 	return (
 		<StyledClassCard>
 			<div className='cardHeader'>
 				<h3>Card Name</h3>
-				<button></button>
+				<button onClick={onDelete}></button>
+				<button onClick={onRegister}></button>
 			</div>
 			<div className='divider'></div>
 			<StyledCardDetail>
-				<h6>Label</h6>
-				<p>Value</p>
+				<h6>Type</h6>
+				<p>{type}</p>
+			</StyledCardDetail>
+			<StyledCardDetail>
+				<h6>Start</h6>
+				<p>{start}</p>
+			</StyledCardDetail>
+			<StyledCardDetail>
+				<h6>Duration</h6>
+				<p>{duration}</p>
+			</StyledCardDetail>
+			<StyledCardDetail>
+				<h6>Intensity Level</h6>
+				<p>{intensity}</p>
+			</StyledCardDetail>
+			<StyledCardDetail>
+				<h6>Location</h6>
+				<p>{location}</p>
+			</StyledCardDetail>
+			<StyledCardDetail>
+				<h6>Attendees</h6>
+				<p>{currentCt}/{maxCt}</p>
 			</StyledCardDetail>
 		</StyledClassCard>
 	);
