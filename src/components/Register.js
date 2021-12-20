aa55137cb65b06e29a06928a56d76646d8bc0b80
ambrojessica <ambrojessica0@gmail.com>
2021-12-20 14:47:51 -0600

    form is done, creating toggle button

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,40 @@ import React from "react";
 const Register = () => {
   return (
     <div>
-      <p>hi</p>
+      <h2>Sign Up</h2>
+      <form>
+        <div className="name">
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            id="name"
+          />
+        </div>
+
+        <div className="email">
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            id="email"
+          />
+        </div>
+
+        <div className="password">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+          />
+        </div>
+
+        <button id="submit">Create Account</button>
+      </form>
     </div>
   );
 };
