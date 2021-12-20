4876f2755c29b67c1a08d360ee2ebc270ebb24e4
jacobjolibois <jacobjolibois@hey.com>
2021-12-20 16:29:13 -0600

    Set up the initial functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
+// Import dependencies
+import React, { useEffect, useState } from 'react';
 import './App.css';
+// Import components
+import ClassList from './components/card-list';
+
+// Set defaults
+const defaultClassList = [];
 
 function App() {
+  // Set state
+  const [ classes, setClasses ] = useState(defaultClassList);
+
+  // Functions
+  useEffect(() => {
+    const getClasses = () => {
+    axios.get('url')
+      .then(res => {
+        console.log(res.data);
+        setClasses(res.data);
+      })
+      .catch(err => console.error(err));
+    }
+  }, [])
+
+  const addClass = newClass => {
+    axios.post('url', newClass)
+      .then(res => {
+        setClasses([res.data, ...classes])
+      })
+  }
+
+  const deleteClass = id => {
+    // Instructor's 'delete class' function
+    axios.delete('url')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.error(err))
+  }
+  
+  const registerClass = () => {
+    // User's 'register for class' function
+  }
+
   return (
     <div className="App">
       {/* Insert Nav Component Here */}
       <div className='container'>
-        <h1>Anywhere Fitness</h1>
-        {/* Insert Body Content Here */}
+        <div className='card'>
+          <div className='classHeader'>
+            <h2>Classes</h2>
+            <button>Add New Classes</button>
+          </div>
+          <ClassList deleteClass={deleteClass} registerClass={registerClass} />
+        </div>
       </div>
     </div>
   );
