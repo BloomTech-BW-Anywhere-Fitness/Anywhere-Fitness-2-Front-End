88eb00de33b1da4857c06b5a85b1975c1d99f08d
jacobjolibois <jacobjolibois@hey.com>
2021-12-20 15:07:37 -0600

    set up global css styles and added a container to the main app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* Insert Nav Component Here */}
+      <div className='container'>
         <h1>Anywhere Fitness</h1>
+        {/* Insert Body Content Here */}
+      </div>
     </div>
   );
 }
