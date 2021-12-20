b475d455ffd70ad6d5c7de21e19aa0f1d76a8bd5
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-20 15:36:54 -0500

    built a basic handleChange function, not srue I'm doing state right, need to wait for backend info to see exactly what the state needs to be.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () =>{
+const [state, setState] = useState({
+    username: '',
+    password: '',
+})
+
+const onSubmit = e =>{
+    e.preventDefault();
+}
+
+const handleChange = e =>{
+    // setState({
+    //     ...state,
+    //     e.target.name: e.target.value
+    // })
+    // console.log(state)
+}
+
     return(
         <div>
             <h2>Sign In</h2>
-            <form>
+            <form onSubmit = {onSubmit}>
  
                 <div>
                     <label>Email</label>
                     <input
+                        name = 'email'
                         type = 'email'
                         placeholder = 'Email'
                         required = {true}
+                        onChange = {handleChange}
                     />
                 </div>
 
                 <div>
                     <label>Password</label>
                     <input
+                            name = 'password'
                             type= 'password'
                             placeholder= 'password'
                             required = {true}
+                            onChange = {handleChange}
                         />
                 </div>
 
