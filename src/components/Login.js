d7bff802642922ad76caaf2abcd0420a4c88c0dd
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-20 15:16:51 -0500

    have the basic JSX of Login.js done

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,34 @@ import React from "react";
 
 const Login = () =>{
     return(
-        <h1>Blah Blah Blah</h1>
+        <div>
+            <h2>Sign In</h2>
+            <form>
+ 
+                <div>
+                    <label>Email</label>
+                    <input
+                        type = 'email'
+                        placeholder = 'Email'
+                        required = {true}
+                    />
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input
+                            type= 'password'
+                            placeholder= 'password'
+                            required = {true}
+                        />
+                </div>
+
+                <button type = 'submit'>Log In</button>
+
+                <p>Don't have an account? <button>Sign Up</button> </p> 
+            </form>
+        </div>
+ 
     )
 }
 
