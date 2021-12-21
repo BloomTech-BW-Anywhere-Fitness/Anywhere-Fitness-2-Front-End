b2be48ae36f6fb583237d6ad68439f6698841971
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-20 19:13:41 -0500

    test

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 // Import styling
 import './App.css';
-<<<<<<< HEAD
-import Login from './components/Login';
-=======
 // Import components
 import ClassList from './components/class-list';
 
 // Set defaults
 const defaultClassList = [];
->>>>>>> 8592e6f7740de8188f0bad14ac01941582f2c828
 
 function App() {
   // Set state
@@ -51,10 +47,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-        <h1>Anywhere Fitness</h1>
-        <Login/>
-=======
       {/* Insert Nav Component Here */}
       <div className='container'>
         <div className='card'>
@@ -65,7 +57,6 @@ function App() {
           <ClassList classes={classes} deleteClass={deleteClass} registerClass={registerClass} />
         </div>
       </div>
->>>>>>> 8592e6f7740de8188f0bad14ac01941582f2c828
     </div>
   );
 }
