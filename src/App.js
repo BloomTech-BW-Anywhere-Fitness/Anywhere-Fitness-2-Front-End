0f86befe049d92029a50d368f2556e620353b968
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-20 19:46:34 -0500

    added correct router syntax to index.js. Now on to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {Router, Switch} from 'react-router'
+import {BrowserRouter, Route} from 'react-router-dom';
 // Import styling
 import './App.css';
 // Import components
@@ -60,6 +61,7 @@ function App() {
       </div>
     </div>
   );
+
 }
 
 export default App;
