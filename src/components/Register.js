bce028896e7bc719463b56d9e640c717da9dc77e
ambrojessica <ambrojessica0@gmail.com>
2021-12-21 13:59:34 -0600

    added styling

--- src/components/Register.js
+++ src/components/Register.js
@@ -40,6 +40,41 @@ const StyledRegister = styled.div`
 .toggle-switch input[type="checkbox"]:checked + .switch {
   background-color: #336699;
 }
+
+.container {
+  border: 1px solid white;
+  border-radius: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 30%;
+  margin-left: 35%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 0 20% 20% 20%;
+  width: 70%;
+}
+
+button{
+  border-radius: 12%;
+  width: 100%;
+  padding: 5%;
+  margin-top: 5%;
+  background-color: black;
+  color: white;
+}
+
+span, label {
+  font-weight: 700;
+}
+
 `;
 
 const Register = () => {
@@ -54,62 +89,66 @@ const Register = () => {
   return (
     <StyledRegister>
       <div>
-        <h2>Sign Up</h2>
-        <form>
-          <div className="name">
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Full Name"
-              name="name"
-              id="name"
-            />
-          </div>
-
-          <div className="email">
-            <label>Email</label>
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              id="email"
-            />
-          </div>
-
-          <div className="password">
-            <label>Password</label>
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              id="password"
-            />
-          </div>
-
-          <div>
-            <p>Register as an instructor</p>
-            <label className="toggle-switch">
-              <input type="checkbox" onClick={toggleOn} />
-              <span className="switch" />
-            </label>
-            {on &&
-              <div>
-                <label>Instructor Authentication Code</label>
+        <div className="container">
+          <h2>Sign Up</h2>
+          <form>
+            <div className="col">
+              <div className="name">
+                <label>Name</label>
                 <input
                   type="text"
-                  placeholder="Authentication Code"
-                  id="auth"
+                  placeholder="Full Name"
+                  name="name"
+                  id="name"
                 />
               </div>
-            }
-          </div>
 
-          <button id="submit">Create Account</button>
+              <div className="email">
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  id="email"
+                />
+              </div>
 
-          <div className="login">
-            <p>Already have an account? <span>Log In</span></p>
-          </div>
-        </form>
+              <div className="password">
+                <label>Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  id="password"
+                />
+              </div>
+
+              <div>
+                <p>Register as an instructor</p>
+                <label className="toggle-switch">
+                  <input type="checkbox" onClick={toggleOn} />
+                  <span className="switch" />
+                </label>
+                {on &&
+                  <div>
+                    <label>Instructor Authentication Code</label>
+                    <input
+                      type="text"
+                      placeholder="Authentication Code"
+                      id="auth"
+                    />
+                  </div>
+                }
+              </div>
+
+              <button id="submit">Create Account</button>
+
+              <div className="login">
+                <p>Already have an account? <span>Log In</span></p>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </StyledRegister>
   );
